5fd0fc9d9503e8ee33490a6f18c6a041824d10dc
Claudio Neto <claudio.neto@alcans.com.br>
2025-01-24 12:57:08 -0300

    adicionado middlewares para validar se cookie esta presente

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,28 +2,41 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select()
 
     return { transactions }
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies
+
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getTransactionParamsSchema.parse(request.params)
 
-    const transaction = await knex('transactions').where('id', id).first()
+    const transaction = await knex('transactions').where({
+      id,
+      session_id: sessionId
+    }).first()
 
     return transaction
   })
 
-  app.get('/summary', async () => {
+  app.get('/summary', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies
+
     const summary = await knex('transactions')
+      .where('session_id', sessionId)
       .sum('amount', { as: 'amount' })
       .first()
 
@@ -46,8 +59,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
       sessionId = randomUUID()
       response.cookie('sessionId', sessionId, {
         path: '/',
-        maxAge: 1000 * 60 * 60 * 24 * 7, //days
-        
+        maxAge: 1000 * 60 * 60 * 24 * 7, // days
       })
     }
 
