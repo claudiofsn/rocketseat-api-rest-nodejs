f98b67f0b045d3d745909c29fed9775168679f48
Claudio Neto <claudio.neto@alcans.com.br>
2025-01-24 12:22:20 -0300

    criado rota para buscar transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,25 +1,43 @@
-import { FastifyInstance } from "fastify"
-import { knex } from "../database"
-import { z } from "zod"
-import { randomUUID } from "node:crypto"
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-    app.post('/', async (request, response) => {
-        const createTransactionBodySchema = z.object({
-            title: z.string(),
-            amount: z.number(),
-            type: z.enum(['credit', 'debit'])
-        })
-        
-        const {title, amount, type} = request.body = createTransactionBodySchema.parse(request.body)
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
 
-        await knex('transactions')
-        .insert({
-            id: randomUUID(),
-            title,
-            amount: type === 'credit' ? amount : amount * -1
-        })
-   
-        return response.status(201).send()
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
     })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return transaction
+  })
+
+  app.post('/', async (request, response) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = (request.body =
+      createTransactionBodySchema.parse(request.body))
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return response.status(201).send()
+  })
 }
