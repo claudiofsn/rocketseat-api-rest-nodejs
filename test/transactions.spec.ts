0fbb361f4d243af26a0ca33c1a2479b74f335aa2
claudio.neto <craudiofelix@gmail.com>
2025-01-28 13:06:45 -0300

    feat:criado banco de testes

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,7 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
 import { server } from '../src/app'
 import request from 'supertest'
+import { execSync } from 'node:child_process'
 
 describe('Transactions routes', () => {
     beforeAll(async () => {
@@ -9,6 +10,11 @@ describe('Transactions routes', () => {
 
     afterAll(async () => {
         await server.close()
+    })
+
+    beforeEach(() => {
+        execSync('npm run knex migrate:rollback --all');
+        execSync('npm run knex migrate:latest')
     })
 
     it('should be able to create a new transactions', async () => {
@@ -20,5 +26,30 @@ describe('Transactions routes', () => {
                 type: 'credit'
             })
             .expect(201)
+    })
+
+    it('should be able to list all transactions', async () => {
+        const createTransactionResponse = await request(server.server)
+            .post('/transactions')
+            .send({
+                title: "Making a test",
+                amount: 5000,
+                type: 'credit'
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionsResponse = await request(server.server)
+            .get('/transactions')
+            .set('Cookie', cookies!)
+            .expect(200)
+
+        expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: "Making a test",
+                amount: 5000,
+            })
+        ])
+
     })
 })
